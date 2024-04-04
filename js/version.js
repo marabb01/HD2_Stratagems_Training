34b45d6cd986cebf346c02749c586ab5ce1ddce2
Martin "Darth <mart.abberley@gmail.com>
2024-04-04 21:28:08 +0100

    Fixing random text if image doesnt load

--- js/version.js
+++ js/version.js
@@ -1,2 +1,2 @@
 // TODO: This should be replaced with some automated semantic versioning system
-const APP_VERSION = '0.0.1'; // Example version number
+const APP_VERSION = '0.0.2'; // Example version number
