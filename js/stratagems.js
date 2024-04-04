41b3db3929abac6dec94919864daf250a0775c6e
Martin "Darth <mart.abberley@gmail.com>
2024-04-04 20:37:35 +0100

    Splitting stratagem array into separate file

---
+++ js/stratagems.js
@@ -0,0 +1,260 @@
+function getStratagems() {
+    return [
+        // Supply - Support Weapons (Patriotic Administration Center)
+        {
+            name: 'MG-43 Machine Gun',
+            icon: 'img/mg-43_machine_gun.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Right']
+        },
+        {
+            name: 'APW-1 Anti-Materiel Rifle',
+            icon: 'img/apw-1_anti-materiel_rifle.jpg',
+            sequence: ['Down', 'Left', 'Right', 'Up', 'Down']
+        },
+        {
+            name: 'M-105 Stalwart',
+            icon: 'img/m-105_stalwart.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Up', 'Left']
+        },
+        {
+            name: 'EAT-17 Expendable Anti-tank',
+            icon: 'img/eat-17_expendable_anti-tank.jpg',
+            sequence: ['Down', 'Down', 'Left', 'Up', 'Right']
+        },
+        {
+            name: 'GR-8 Recoilless Rifle',
+            icon: 'img/gr-8_recoilless_rifle.jpg',
+            sequence: ['Down', 'Left', 'Right', 'Right', 'Left']
+        },
+        {
+            name: 'FLAM-40 Flamethrower',
+            icon: 'img/flam-40_flamethrower.jpg',
+            sequence: ['Down', 'Left', 'Up', 'Down', 'Up']
+        },
+        {
+            name: 'AC-8 Autocannon',
+            icon: 'img/ac-8_autocannon.jpg',
+            sequence: ['Down', 'Right', 'Left', 'Down', 'Down', 'Up', 'Up', 'Right']
+        },
+        {
+            name: 'MG-206 Heavy Machine Gun',
+            icon: 'img/mg-206_heavy_machine_gun.jpg',
+            sequence: ['Down', 'Left', 'Up', 'Down', 'Down']
+        },
+        {
+            name: 'RS-422 Railgun',
+            icon: 'img/rs-422_railgun.jpg',
+            sequence: ['Down', 'Right', 'Left', 'Down', 'Down', 'Up', 'Left', 'Down', 'Right']
+        },
+        {
+            name: 'FAF-14 SPEAR Launcher',
+            icon: 'img/faf-14_spear_launcher.jpg',
+            sequence: ['Down', 'Down', 'Up', 'Down', 'Down']
+        },
+
+        // All Orbital Cannons (Orbital)
+        {
+            name: 'Orbital Gatling Barrage',
+            icon: 'img/orbital_gatling_barrage.jpg',
+            sequence: ['Right', 'Down', 'Left', 'Up', 'Up']
+        },
+        {
+            name: 'Orbital Airburst Strike',
+            icon: 'img/orbital_airburst_strike.jpg',
+            sequence: ['Right', 'Right', 'Right']
+        },
+        {
+            name: 'Orbital 120MM HE Barrage',
+            icon: 'img/orbital_120mm_he_barrage.jpg',
+            sequence: ['Right', 'Down', 'Down', 'Left', 'Down', 'Right', 'Down', 'Down']
+        },
+        {
+            name: 'Orbital 380MM HE Barrage',
+            icon: 'img/orbital_380mm_he_barrage.jpg',
+            sequence: ['Right', 'Down', 'Down', 'Up', 'Up', 'Left', 'Down', 'Down', 'Down']
+        },
+        {
+            name: 'Orbital Walking Barrage',
+            icon: 'img/orbital_walking_barrage.jpg',
+            sequence: ['Right', 'Down', 'Right', 'Down', 'Right', 'Down']
+        },
+        {
+            name: 'Orbital Laser',
+            icon: 'img/orbital_laser.jpg',
+            sequence: ['Right', 'Up', 'Left', 'Up', 'Right', 'Left']
+        },
+        {
+            name: 'Orbital Railcannon Strike',
+            icon: 'img/orbital_railcannon_strike.jpg',
+            sequence: ['Right', 'Down', 'Up', 'Down', 'Left']
+        },
+
+        // All Hangar (Eagle)
+        {
+            name: 'Eagle Strafing Run',
+            icon: 'img/eagle_strafing_run.jpg',
+            sequence: ['Up', 'Right', 'Right']
+        },
+        {
+            name: 'Eagle Airstrike',
+            icon: 'img/eagle_airstrike.jpg',
+            sequence: ['Up', 'Right', 'Down', 'Right']
+        },
+        {
+            name: 'Eagle Cluster Bomb',
+            icon: 'img/eagle_cluster_bomb.jpg',
+            sequence: ['Up', 'Right', 'Down', 'Down', 'Right', 'Down']
+        },
+        {
+            name: 'Eagle Napalm Airstrike',
+            icon: 'img/eagle_napalm_airstrike.jpg',
+            sequence: ['Up', 'Right', 'Down', 'Up']
+        },
+        {
+            name: 'LIFT-850 Jump Pack',
+            icon: 'img/lift-850_jump_pack.jpg',
+            sequence: ['Down', 'Up', 'Up', 'Down', 'Up']
+        },
+        {
+            name: 'Eagle Smoke Strike',
+            icon: 'img/eagle_smoke_strike.jpg',
+            sequence: ['Up', 'Right', 'Up', 'Down']
+        },
+        {
+            name: 'Eagle 110MM Rocket Pods',
+            icon: 'img/eagle_110mm_rocket_pods.jpg',
+            sequence: ['Up', 'Down', 'Up', 'Left']
+        },
+        {
+            name: 'Eagle 500KG Bomb',
+            icon: 'img/eagle_500kg_bomb.jpg',
+            sequence: ['Up', 'Left', 'Down', 'Down', 'Down']
+        },
+
+        // Bridge Stratagems
+        {
+            name: 'Orbital Precision Strike',
+            icon: 'img/orbital_precision_strike.jpg',
+            sequence: ['Right', 'Right', 'Up']
+        },
+        {
+            name: 'Orbital Gas Strike',
+            icon: 'img/orbital_gas_strike.jpg',
+            sequence: ['Right', 'Right', 'Down', 'Right']
+        },
+        {
+            name: 'Orbital EMS Strike',
+            icon: 'img/orbital_ems_strike.jpg',
+            sequence: ['Right', 'Right', 'Left', 'Down']
+        },
+        {
+            name: 'Orbital Smoke Strike',
+            icon: 'img/orbital_smoke_strike.jpg',
+            sequence: ['Right', 'Right', 'Down', 'Up']
+        },
+        {
+            name: 'E/MG-101 HMG Emplacement',
+            icon: 'img/e_mg-101_hmg_emplacement.jpg',
+            sequence: ['Up', 'Down', 'Left', 'Right', 'Right', 'Left']
+        },
+        {
+            name: 'FX-12 Shield Generator Relay',
+            icon: 'img/fx-12_shield_generator_relay.jpg',
+            sequence: ['Down', 'Up', 'Left', 'Right', 'Left', 'Down']
+        },
+        {
+            name: 'A/ARC-3 Tesla Tower',
+            icon: 'img/a_arc-3_tesla_tower.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Up', 'Left', 'Right']
+        },
+
+        // Engineering Bay
+        {
+            name: 'MD-6 Anti-Personnel Minefield',
+            icon: 'img/md-6_anti-personnel_minefield.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Right']
+        },
+        {
+            name: 'B-1 Supply Pack',
+            icon: 'img/b-1_supply_pack.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Up', 'Down']
+        },
+        {
+            name: 'GL-21 Grenade Launcher',
+            icon: 'img/gl-21_grenade_launcher.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Left', 'Down', 'Down']
+        },
+        {
+            name: 'LAS-98 Laser Cannon',
+            icon: 'img/las-98_laser_cannon.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Left']
+        },
+        {
+            name: 'MD-14 Incendiary Mines',
+            icon: 'img/md-i4_incendiary_mines.jpg',
+            sequence: ['Down', 'Left', 'Left', 'Down']
+        },
+        {
+            name: 'AX/LAS-5 "Guard Dog" Rover',
+            icon: 'img/ax_las-5_guard_dog_rover.jpg',
+            sequence: ['Down', 'Left', 'Down', 'Up', 'Left', 'Down', 'Down']
+        },
+        {
+            name: 'SH-20 Ballistic Shield Backpack',
+            icon: 'img/sh-20_ballistic_shield_backpack.jpg',
+            sequence: ['Down', 'Left', 'Up', 'Up', 'Right']
+        },
+        {
+            name: 'ARC-3 Arc Thrower',
+            icon: 'img/arc-3_arc_thrower.jpg',
+            sequence: ['Down', 'Right', 'Up', 'Left', 'Down']
+        },
+        {
+            name: 'LAS-99 Quasar Cannon',
+            icon: 'img/las-99_quasar_cannon.jpg',
+            sequence: ['Down', 'Down', 'Up', 'Left', 'Right']
+        },
+        {
+            name: 'SH-32 Shield Generator Pack',
+            icon: 'img/sh-32_shield_generator_pack.jpg',
+            sequence: ['Down', 'Up', 'Left', 'Down', 'Right', 'Right']
+        },
+
+        // Robotics Workshop
+        {
+            name: 'A/MG-43 Machine Gun Sentry',
+            icon: 'img/a_mg-43_machine_gun_sentry.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Down', 'Right', 'Down', 'Up']
+        },
+        {
+            name: 'A/G-16 Gatling Sentry',
+            icon: 'img/a_g-16_gatling_sentry.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Left', 'Down']
+        },
+        {
+            name: 'A/M-12 Mortar Sentry',
+            icon: 'img/a_m-12_mortar_sentry.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Right', 'Down']
+        },
+        {
+            name: 'AX/AR-23 "Guard Dog"',
+            icon: 'img/ax_ar-23_guard_dog.jpg',
+            sequence: ['Down', 'Up', 'Left', 'Down', 'Up', 'Right', 'Down']
+        },
+        {
+            name: 'A/AC-8 Autocannon Sentry',
+            icon: 'img/a_ac-8_autocannon_sentry.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Up', 'Left', 'Up']
+        },
+        {
+            name: 'A/MLS-4X Rocket Sentry',
+            icon: 'img/a_mls-4x_rocket_sentry.jpg',
+            sequence: ['Down', 'Up', 'Right', 'Right', 'Left']
+        },
+        {
+            name: 'A/M-23 EMS Mortar Sentry',
+            icon: 'img/a_m-23_ems_mortar_sentry.jpg',
+            sequence: ['Down', 'Down', 'Up', 'Up', 'Left']
+        },
+    ];
+}
