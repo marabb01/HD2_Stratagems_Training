75edf5fbf6c9cf05b2f5f9d021129c36591cac02
Martin "Darth <mart.abberley@gmail.com>
2024-04-04 23:52:55 +0100

    code refactor

--- js/script.js
+++ js/script.js
@@ -1,136 +1,135 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Display the version banner
+document.addEventListener('DOMContentLoaded', () => {
+    initPageElements();
+    setupGameControls();
+});
+
+function initPageElements() {
     const versionBanner = document.getElementById('version-banner');
-    if (versionBanner) {
-        versionBanner.innerHTML = `Version: ${APP_VERSION}`;
-    }
-
-    // Update feedback link
     const feedbackLink = document.getElementById('feedback-link');
+
+    if (versionBanner) versionBanner.textContent = `Version: ${APP_VERSION}`;
     if (feedbackLink) {
-        feedbackLink.innerHTML = `<a href="https://github.com/marabb01/HD2_Stratagems_Training/issues" target="_blank">Feedback</a>`;
+        feedbackLink.href = "https://github.com/marabb01/HD2_Stratagems_Training/issues";
+        feedbackLink.target = "_blank";
+        feedbackLink.textContent = "Feedback";
     }
-});
+}
 
+function setupGameControls() {
+    document.getElementById('controlToggle').addEventListener('change', stratagemSelection);
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Enter' && canStartNewGame) stratagemSelection();
+    });
+}
 
-let canStartNewGame = true; // Boolean flag to control game start
-
-// Add an event listener for the control toggle checkbox
-document.getElementById('controlToggle').addEventListener('change', function() {
-    // Re-initialize the game to apply the new control scheme, if needed
-    stratagemSelection();
-});
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter' && canStartNewGame) {
-        stratagemSelection();
-    }
-});
+let canStartNewGame = true;
 
 function stratagemSelection() {
-    canStartNewGame = false; // Prevent new game start
-    document.removeEventListener('keydown', handleKeyDown);
-    document.getElementById('feedback').textContent = '';
-    document.getElementById('trauma-guy').style.display = 'none';
-    document.getElementById('helldivers-logo').style.display = 'none';
-    const stratagems = getStratagems()
+    canStartNewGame = false;
+    const elementsToHide = ['feedback', 'trauma-guy', 'helldivers-logo'];
+    elementsToHide.forEach(id => document.getElementById(id).style.display = 'none');
 
-    // Select a stratagem at random
-    let selectedStratagem = stratagems[Math.floor(Math.random() * stratagems.length)];
+    const selectedStratagem = getStratagems()[Math.floor(Math.random() * getStratagems().length)];
     displayStratagemInfo(selectedStratagem);
 
-    let startTime = performance.now()
-    let correctInput = selectedStratagem.sequence;
-    let currentInput = [];
-    let timerStarted = false; // Flag to track if the timer has started
+    const gameSession = new GameSession(selectedStratagem.sequence);
+    gameSession.init();
+}
 
-    // Update the timer every 100 milliseconds for accuracy
-    let timerInterval; // Declare timerInterval here but don't start it yet
+function displayStratagemInfo(stratagem) {
+    document.getElementById('stratagem-name').textContent = stratagem.name;
+    const iconElement = document.getElementById('stratagem-icon');
+    iconElement.src = stratagem.icon;
+    iconElement.style.display = 'block';
 
-    document.getElementById('timer').textContent = ``
+    const sequenceDisplay = document.getElementById('stratagem-display');
+    sequenceDisplay.innerHTML = '';
 
-    document.addEventListener('keydown', handleKeyDown);
+    stratagem.sequence.forEach(direction => {
+        const imgElement = createDirectionIcon(direction);
+        sequenceDisplay.appendChild(imgElement);
+    });
+}
 
-    function handleKeyDown(event) {
-        if (event.key === 'Enter') return; // Ignore Enter key
+function createDirectionIcon(direction) {
+    const isWASD = document.getElementById('controlToggle').checked;
+    const img = document.createElement('img');
+    img.src = isWASD ? `img/${direction.toLowerCase()}_icon.jpg` : `img/arrow_${direction.toLowerCase()}.jpg`;
+    img.alt = direction;
+    img.style.width = '50px';
+    img.style.margin = '5px';
+    return img;
+}
 
-        let keyPressed = mapKey(event.key);
-        if (keyPressed === null) return; // Ignore inputs other than WASD, arrow keys
+class GameSession {
+    constructor(correctInput) {
+        this.correctInput = correctInput;
+        this.currentInput = [];
+        this.timerStarted = false;
+        this.startTime = null;
+        this.timerInterval = null;
+    }
 
-        if (!timerStarted) {
-            startTime = performance.now(); // Reset startTime at the first key press
-            timerInterval = setInterval(function() { // Start the timer only once
-                let elapsedTime = (performance.now() - startTime) / 1000; // Convert milliseconds to seconds
-                document.getElementById('timer').textContent = `Time: ${elapsedTime.toFixed(3)}s`;
-            }, 10);
-            timerStarted = true; // Prevent the timer from starting again
-        }
+    init() {
+        document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    }
 
-        if (currentInput.length < correctInput.length) {
-            currentInput.push(keyPressed);
-            let currentIndex = currentInput.length - 1;
-            let arrows = document.querySelectorAll('#stratagem-display img');
+    handleKeyDown(event) {
+        if (event.key === 'Enter') this.resetGameSession();
 
-            if (correctInput[currentIndex] === keyPressed) {
-                arrows[currentIndex].style.filter = 'drop-shadow(0 0 5px green)';
-            } else {
-                arrows[currentIndex].style.filter = 'drop-shadow(0 0 5px red)';
-                document.getElementById('feedback').textContent = "Incorrect - Press ENTER for another try!";
-                document.removeEventListener('keydown', handleKeyDown);
-                canStartNewGame = true; // Allow for new game start
-                clearInterval(timerInterval); // Stop the timer
-                return;
-            }
+        const keyPressed = this.mapKey(event.key);
+        if (!keyPressed) return;
 
-            if (currentInput.length === correctInput.length) {
-                clearInterval(timerInterval); // Stop the timer
-                document.getElementById('feedback').textContent = `Correct - Press ENTER for another try!`;
-                document.removeEventListener('keydown', handleKeyDown);
-                canStartNewGame = true; // Allow for new game start
-            }
+        this.startTimerIfNeeded();
+
+        this.currentInput.push(keyPressed);
+        this.checkInputAccuracy();
+    }
+
+    startTimerIfNeeded() {
+        if (!this.timerStarted) {
+            this.startTime = performance.now();
+            this.timerStarted = true;
+            this.timerInterval = setInterval(() => this.updateTimer(), 10);
         }
     }
 
-    document.addEventListener('keydown', handleKeyDown);
+    updateTimer() {
+        const elapsedTime = (performance.now() - this.startTime) / 1000;
+        document.getElementById('timer').textContent = `Time: ${elapsedTime.toFixed(3)}s`;
+    }
 
-    function mapKey(keyPressed) {
-        let isWASD = document.getElementById('controlToggle').checked;
-        if (isWASD) {
-            switch (keyPressed) {
-                case 'w': return 'Up';
-                case 'a': return 'Left';
-                case 's': return 'Down';
-                case 'd': return 'Right';
-                default: return null; // Change to ignore other keys
-            }
+    checkInputAccuracy() {
+        const currentIndex = this.currentInput.length - 1;
+        const arrows = document.querySelectorAll('#stratagem-display img');
+
+        if (this.correctInput[currentIndex] === this.currentInput[currentIndex]) {
+            arrows[currentIndex].style.filter = 'drop-shadow(0 0 5px green)';
+            if (this.currentInput.length === this.correctInput.length) this.endGame(true);
         } else {
-            switch (keyPressed) {
-                case 'ArrowUp': return 'Up';
-                case 'ArrowDown': return 'Down';
-                case 'ArrowLeft': return 'Left';
-                case 'ArrowRight': return 'Right';
-                default: return null; // Change to ignore other keys
-            }
+            arrows[currentIndex].style.filter = 'drop-shadow(0 0 5px red)';
+            this.endGame(false);
         }
     }
 
-    function displayStratagemInfo(stratagem) {
-        document.getElementById('stratagem-name').textContent = stratagem.name;
-        let iconElement = document.getElementById('stratagem-icon');
-        iconElement.src = stratagem.icon;
-        iconElement.style.display = 'block';
+    endGame(isSuccess) {
+        clearInterval(this.timerInterval);
+        const feedback = document.getElementById('feedback');
+        feedback.textContent = isSuccess ? "Correct - Press ENTER for another try!" : "Incorrect - Press ENTER for another try!";
+        canStartNewGame = true;
+        document.removeEventListener('keydown', this.handleKeyDown.bind(this));
+    }
 
-        let sequenceDisplay = document.getElementById('stratagem-display');
-        let isWASD = document.getElementById('controlToggle').checked;
-        sequenceDisplay.innerHTML = '';
-        stratagem.sequence.forEach(direction => {
-            let img = document.createElement('img');
-            // Choose the icon set based on the control scheme
-            img.src = isWASD ? `img/${direction.toLowerCase()}_icon.jpg` : `img/arrow_${direction.toLowerCase()}.jpg`;
-            img.alt = direction;
-            img.style.width = '50px';
-            img.style.margin = '5px';
-            sequenceDisplay.appendChild(img);
-        });
+    resetGameSession() {
+        if (canStartNewGame) stratagemSelection();
+    }
+
+    mapKey(keyPressed) {
+        const isWASD = document.getElementById('controlToggle').checked;
+        const keyMap = {
+            w: 'Up', a: 'Left', s: 'Down', d: 'Right',
+            ArrowUp: 'Up', ArrowDown: 'Down', ArrowLeft: 'Left', ArrowRight: 'Right'
+        };
+        return isWASD ? keyMap[keyPressed.toLowerCase()] : keyMap[keyPressed];
     }
 }
